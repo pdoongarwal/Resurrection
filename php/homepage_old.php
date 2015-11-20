ff70dbd6b8d40570c6f32057bffb226cc8cb3619
pdoongarwal <pdoongarwal@gmail.com>
2015-11-20 23:06:41 +0530

    Adding login and signup feature

---
+++ php/homepage_old.php
@@ -0,0 +1,45 @@
+<html>
+
+	<head>
+		<title>CF-Toolkit</title>
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../css/homepage.css" rel="stylesheet">
+		<script src="../js/html5shiv.min.js"></script>
+	    <script src="../js/respond.min.js"></script>
+	</head>
+
+	<body>
+
+		<nav class = "navbar navbar-inverse navbar-fixed-top" id ="my-navbar">
+			<div class = "container-fluid">
+			    <div class = "navbar-header">
+			      	<button type="button" class ="navbar-toggle" data-toggle = "collapse" data-target ="#navbar-collapse">
+				    	<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+				  	</button>
+		         	<a href="homepage.php" class="navbar-brand">CF-Toolkit</a>	
+			    </div>
+		       
+		      	<div class="collapse navbar-collapse" id="navbar-collapse">
+		        	<a href="Signup.php" class="btn btn-warning navbar-btn navbar-right">Login</a>
+		         	<ul class="nav navbar-nav">
+				        <li><a href="compete.php">Compete</a>
+				        <li><a href='#gallery'>All Submissions</a>
+				        <li><a href="#calendar">Last Submissions</a>
+				        <li><a href="#feature">Compare</a>
+				        <li><a href="#contact">Bug-e-Code</a>
+				        <li><a href="#notice">Notice</a>
+					</ul>
+		      	</div>
+		    </div>
+	   	</nav>
+
+	   	<img src = "../images/wall.jpg" width='100%'>
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <script src="../js/bootstrap.min.js"></script>
+	</body>
+</html>
